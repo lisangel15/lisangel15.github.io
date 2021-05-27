cd93910211dc4bbe182271e290a644bb8b627775
Lisangel Herra <lisangelherra@gmail.com>
2021-05-26 22:20:03 -0400

    education file change

--- lisangel15.github.io/src/contents/Education.js
+++ lisangel15.github.io/src/contents/Education.js
@@ -5,8 +5,7 @@ render() {
 return (
 <div className="condiv">
 <h1 className="subtopic">My Education</h1>
-<Widecard title="M.Tech Software Engineering" where="VIT University" from="July 2017" to="Present"/>
-<Widecard title="SSLC | HSC" where="Islamiah Higher Secondary School" from="2011" to="2017"/>
+
 </div>
 )
 }
