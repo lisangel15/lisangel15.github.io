623b80d17c1ae28af737b61cf8f083713eca57fd
lisangel15 <54686606+lisangel15@users.noreply.github.com>
2021-05-26 21:25:52 -0400

    Add files via upload

---
+++ src/contents/ContactMe.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+
+class ContactMe extends Component {
+render() {
+return (
+<div className="condiv">
+<h1 className="subtopic">Contact Me</h1>
+<h3>Email  :   lisangelherra@gmail.com.com</h3>
+<h3>Linkedin   :   Lisangel Herra</h3>
+<Social />
+</div>
+)
+}
+}
+export default ContactMe
